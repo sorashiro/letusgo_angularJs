b0b66ec0dd598f26f29877fb194d1f1c50d515e7
sorashiro <yufei930101@gmail.com>
2014-09-06 02:38:59 +0800

    update itemsService

--- app/scripts/service/itemsService.js
+++ app/scripts/service/itemsService.js
@@ -2,28 +2,48 @@
 
 angular.module('letusgoAngularJsApp').service('itemsService', function(localStorageService){
 
-    this.items = function(){
+  this.items = function(){
 
-      var itemsList = [
-        {
-          "category": "fruit", "name": "apple", "unit": "斤", "price": "5.50", "quantity": "0"
-        },
-        {
-          "category": "fruit", "name": "leechee", "unit": "斤", "price": "15.00", "quantity": "0"
-        },
-        {
-          "category": "food", "name": "sprite", "unit": "瓶", "price": "3.00", "quantity": "0"
-        },
-        {
-          "category": "food", "name": "coca-cola", "unit": "瓶", "price": "3.00", "quantity": "0"
-        },
-        {
-          "category": "livingGoods", "name": "battery", "unit": "个", "price": "2.00", "quantity": "0"
-        }
-      ];
-      localStorageService.set('itemsList', itemsList);
-      return itemsList;
-    }
+    var itemsList = [
+      {
+        "category": "fruit", "name": "apple", "unit": "斤", "price": "5.50", "quantity": "0"
+      },
+      {
+        "category": "fruit", "name": "leechee", "unit": "斤", "price": "15.00", "quantity": "0"
+      },
+      {
+        "category": "food", "name": "sprite", "unit": "瓶", "price": "3.00", "quantity": "0"
+      },
+      {
+        "category": "food", "name": "coca-cola", "unit": "瓶", "price": "3.00", "quantity": "0"
+      },
+      {
+        "category": "livingGoods", "name": "battery", "unit": "个", "price": "2.00", "quantity": "0"
+      }
+    ];
+    localStorageService.set('itemsList', itemsList);
+    return itemsList;
+  };
+
+  this.count = function(){
+    var count = localStorageService.get(clickCount);
+    localStorageService.set('clickCount', count);
+    return count;
+
+  };
+
+  this.addTocart = function(item){
+
+  };
+
+  this.add = function(key, value){
+    return localStorageService.set(key, value);
+  };
+
+  this.get = function(key){
+    return localStorageService.get(key);
+  };
+
 
 
 });
