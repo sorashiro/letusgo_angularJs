26b89f278d335dd4476ef46b0c3cd178e4144021
sorashiro <yufei930101@gmail.com>
2014-09-05 12:21:15 +0800

    change type to category

--- app/scripts/service/itemsService.js
+++ app/scripts/service/itemsService.js
@@ -6,19 +6,19 @@ angular.module('letusgoAngularJsApp').service('itemsService', function(localStor
 
       var itemsList = [
         {
-          "type": "fruit", "name": "apple", "unit": "斤", "price": "5.50", "quantity": "0"
+          "category": "fruit", "name": "apple", "unit": "斤", "price": "5.50", "quantity": "0"
         },
         {
-          "type": "fruit", "name": "leechee", "unit": "斤", "price": "15.00", "quantity": "0"
+          "category": "fruit", "name": "leechee", "unit": "斤", "price": "15.00", "quantity": "0"
         },
         {
-          "type": "food", "name": "sprite", "unit": "瓶", "price": "3.00", "quantity": "0"
+          "category": "food", "name": "sprite", "unit": "瓶", "price": "3.00", "quantity": "0"
         },
         {
-          "type": "food", "name": "coca-cola", "unit": "瓶", "price": "3.00", "quantity": "0"
+          "category": "food", "name": "coca-cola", "unit": "瓶", "price": "3.00", "quantity": "0"
         },
         {
-          "type": "livingGoods", "name": "battery", "unit": "个", "price": "2.00", "quantity": "0"
+          "category": "livingGoods", "name": "battery", "unit": "个", "price": "2.00", "quantity": "0"
         }
       ];
       localStorageService.set('itemsList', itemsList);
