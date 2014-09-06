a98381706b9102cbaee2e1e4ebb4651f0a6b4880
sorashiro <yufei930101@gmail.com>
2014-09-07 05:04:27 +0800

    change cartItemService

--- app/scripts/service/cartItemService.js
+++ app/scripts/service/cartItemService.js
@@ -1,1 +1,11 @@
 'use strict';
+
+angular.module('letusgoAngularJsApp').service('cartItemService', function(localStorageService){
+
+  this.cartList = function(cartLists){
+    cartLists = localStorageService.get('cartList');
+    return cartLists;
+  };
+
+
+});
