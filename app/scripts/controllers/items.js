53f5345cc41103f05b62b299258c8e3cfd2743c3
sorashiro <yufei930101@gmail.com>
2014-09-07 05:03:53 +0800

    update itemCtrl

--- app/scripts/controllers/items.js
+++ app/scripts/controllers/items.js
@@ -1,12 +1,15 @@
 'use strict';
 
 angular.module('letusgoAngularJsApp').controller('ItemsCtrl', function($scope, itemsService) {
+
   var cartList = [];
   var names = [];
+
   $scope.items = itemsService.items();
   $scope.$emit('_parent_count');
 
   $scope.addToCart = function(item){
+
     $scope.count = itemsService.count();
     $scope.$emit('_parent_count');
     itemsService.addToCart(cartList, names, item);
