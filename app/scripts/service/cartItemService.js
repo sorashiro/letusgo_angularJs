7046a66bf8d92caee4e0938a78d490fa07699b66
sorashiro <yufei930101@gmail.com>
2014-09-09 00:48:12 +0800

    change cartList with every category but not complete..

--- app/scripts/service/cartItemService.js
+++ app/scripts/service/cartItemService.js
@@ -6,12 +6,15 @@ angular.module('letusgoAngularJsApp').service('cartItemService', function(localS
     cartLists = localStorageService.get('cartList');
     return cartLists;
   };
+
   this.category = function(categorys, cartLists){
     cartLists = localStorageService.get('cartList');
     var category;
     for (var i = 0; i < cartLists.length; i++){
       category = cartLists[i].item.category;
-      categorys.push(category);
+      if (categorys.indexOf(category) === -1) {
+        categorys.push(category);
+      }
     }
     for (var k = 0; k < categorys.length; k++){
       var item = [];
