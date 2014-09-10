be8b67b56969330021bf529637d358b56f08f35a
sorashiro <yufei930101@gmail.com>
2014-09-10 23:31:33 +0800

    add addGoods function to goodsInformationCtrl

--- app/scripts/controllers/goodsInformationManager.js
+++ app/scripts/controllers/goodsInformationManager.js
@@ -1,6 +1,18 @@
 'use strict';
 
-angular.module('letusgoAngularJsApp').controller('GoodsInformationCtrl', function($scope, goodsInformationService){
+angular.module('letusgoAngularJsApp').controller('GoodsInformationCtrl', function($scope, categoryService, goodsInformationService){
 
   $scope.loadGoodsInformations = goodsInformationService.loadGoodsInformations();
+  $scope.categorys = categoryService.loadCategory();
+  $scope.categoryName = "分类名称";
+  $scope.getName = function(category){
+    $scope.categoryName = category;
+  };
+  $scope.addGoods = function(){
+
+    var name = $scope.name;
+    var unit = $scope.unit;
+    var price = $scope.price;
+    goodsInformationService.addGoods($scope.categoryName, name, unit, price);
+  }
 });
