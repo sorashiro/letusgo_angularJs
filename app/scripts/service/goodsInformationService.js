ed10c8a8b6307a732ad6c899c5de48dbb1c57683
sorashiro <yufei930101@gmail.com>
2014-09-10 23:32:29 +0800

    add addGoods function to goodsInformationService

--- app/scripts/service/goodsInformationService.js
+++ app/scripts/service/goodsInformationService.js
@@ -5,5 +5,11 @@ angular.module('letusgoAngularJsApp').service('goodsInformationService', functio
   this.loadGoodsInformations = function(){
     var items = itemsService.get('itemsList');
     return items;
-  }
+  };
+
+  this.addGoods = function(category, name, unit, price){
+    var items = this.loadGoodsInformations();
+    items.push({"category": category, "name": name, "unit": unit, "price": price});
+    itemsService.add('itemsList', items);
+  };
 });
