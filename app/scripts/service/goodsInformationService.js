2de530843c6ccb83d964dfbf417dfc3c531caf14
sorashiro <yufei930101@gmail.com>
2014-09-10 22:33:43 +0800

    modify loadItems function in goodsInformationService

--- app/scripts/service/goodsInformationService.js
+++ app/scripts/service/goodsInformationService.js
@@ -3,7 +3,7 @@
 angular.module('letusgoAngularJsApp').service('goodsInformationService', function(localStorageService, itemsService){
 
   this.loadGoodsInformations = function(){
-    var items = itemsService.items();
+    var items = itemsService.get('itemsList');
     return items;
   }
 });
