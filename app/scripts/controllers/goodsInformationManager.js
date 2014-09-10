f047f824deaf8e8991c48c1ffa926c05ee828ccc
sorashiro <yufei930101@gmail.com>
2014-09-11 01:34:18 +0800

    add move function to goodsInformationCtrl

--- app/scripts/controllers/goodsInformationManager.js
+++ app/scripts/controllers/goodsInformationManager.js
@@ -1,18 +1,24 @@
 'use strict';
 
-angular.module('letusgoAngularJsApp').controller('GoodsInformationCtrl', function($scope, categoryService, goodsInformationService){
+angular.module('letusgoAngularJsApp').controller('GoodsInformationCtrl', function($scope, itemsService, categoryService, goodsInformationService){
 
-  $scope.loadGoodsInformations = goodsInformationService.loadGoodsInformations();
+  $scope.loadGoodsInformations = itemsService.loadItems();
   $scope.categorys = categoryService.loadCategory();
   $scope.categoryName = "分类名称";
   $scope.getName = function(category){
     $scope.categoryName = category;
   };
+
   $scope.addGoods = function(){
 
     var name = $scope.name;
     var unit = $scope.unit;
     var price = $scope.price;
     goodsInformationService.addGoods($scope.categoryName, name, unit, price);
+  };
+
+  $scope.remove = function(goodsInformation){
+    goodsInformationService.remove(goodsInformation);
+    $scope.loadGoodsInformations = itemsService.loadItems();
   }
 });
