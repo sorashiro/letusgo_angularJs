5c34fc3e4aea6f4e5f9338b7485e3cfb1755a503
sorashiro <yufei930101@gmail.com>
2014-09-11 01:38:24 +0800

    add remove function to goodsInformationService

--- app/scripts/service/goodsInformationService.js
+++ app/scripts/service/goodsInformationService.js
@@ -2,14 +2,19 @@
 
 angular.module('letusgoAngularJsApp').service('goodsInformationService', function(localStorageService, itemsService){
 
-  this.loadGoodsInformations = function(){
-    var items = itemsService.get('itemsList');
-    return items;
-  };
-
   this.addGoods = function(category, name, unit, price){
-    var items = this.loadGoodsInformations();
+    var items = itemsService.loadItems();
     items.push({"category": category, "name": name, "unit": unit, "price": price});
     itemsService.add('itemsList', items);
   };
+
+  this.remove = function(goodsInformation){
+    var items = itemsService.loadItems();
+    for (var i = 0; i < items.length; i++){
+      if (goodsInformation.name === items[i].name){
+        items.splice(i, 1);
+      }
+    }
+    itemsService.add('itemsList', items);
+  }
 });
