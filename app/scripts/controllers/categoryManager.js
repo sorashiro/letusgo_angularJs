caadd7926945359c14053cd443cf60456f17d014
sorashiro <yufei930101@gmail.com>
2014-09-11 01:32:25 +0800

    add remove function to categoryCtrl

--- app/scripts/controllers/categoryManager.js
+++ app/scripts/controllers/categoryManager.js
@@ -3,8 +3,16 @@
 angular.module('letusgoAngularJsApp').controller('CategoryCtrl', function($scope, categoryService, localStorageService){
 
   $scope.categorys = categoryService.loadCategory();
+
   $scope.addCategory = function(){
     var category = $scope.input;
     categoryService.addCategory(category);
+  };
+
+  $scope.removes = function(category){
+
+    categoryService.removes(category);
+    $scope.categorys = categoryService.loadCategory();
+
   }
 });
