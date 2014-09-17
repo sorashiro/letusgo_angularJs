413a07b6b3d970f2c8ec1f970e040db287c5c13f
sorashiro <yufei930101@gmail.com>
2014-09-17 21:02:57 +0800

    add emit test in category-spce.js

---
+++ test/spec/controllers/category-spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+describe('Controller: ItemsCtrl', function () {
+  var CategoryService, $scope, createController;
+
+  beforeEach(function () {
+    module('letusgoAngularJsApp');
+
+    inject(function ($injector) {
+
+      $scope = $injector.get('$rootScope').$new();
+      CategoryService = $injector.get('CategoryService');
+
+      var $controller = $injector.get('$controller');
+
+      createController = function () {
+        return $controller('CategoryCtrl', {
+          $scope: $scope,
+          CategoryService: CategoryService
+        });
+      };
+    });
+  });
+
+  it('emit from parent controller', function() {
+    spyOn($scope, '$emit');
+    createController();
+    expect($scope.$emit).toHaveBeenCalledWith('parentManage');
+  });
+
+  
+});
