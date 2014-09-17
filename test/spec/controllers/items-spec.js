d74f16245bfcf1a4144c5bc5f9f4af72fa3ff6b4
sorashiro <yufei930101@gmail.com>
2014-09-17 11:52:29 +0800

    add loadItems function test

---
+++ test/spec/controllers/items-spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+describe('Controller: ItemsCtrl', function () {
+  var ItemsService, $scope, createController;
+
+  beforeEach(function () {
+    module('letusgoAngularJsApp');
+
+    inject(function ($injector) {
+
+      $scope = $injector.get('$rootScope').$new();
+      ItemsService = $injector.get('ItemsService');
+
+      var $controller = $injector.get('$controller');
+
+      createController = function () {
+        return $controller('ItemsCtrl', {
+          $scope: $scope,
+          itemService: ItemsService
+        });
+      };
+    });
+  });
+  it('should return all items to items', function () {
+    spyOn(ItemsService, 'loadItems');
+    createController();
+    var result = ItemsService.loadItems();
+    expect($scope.items).toEqual(result);
+  });
+});
