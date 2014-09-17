9b9bbbeb41ba8c627eb41ff4fb1170b75da91d5e
sorashiro <yufei930101@gmail.com>
2014-09-18 02:13:31 +0800

    add emit test

---
+++ test/spec/controllers/modifyCategory-spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+describe('Controller: ModifyCategoryCtrl', function () {
+  var CategoryService, $scope, createController;
+
+  beforeEach(function () {
+    module('letusgoAngularJsApp');
+
+    inject(function ($injector) {
+
+      $scope = $injector.get('$rootScope').$new();
+      CategoryService = $injector.get('CategoryService');
+
+      var $controller = $injector.get('$controller');
+
+      createController = function () {
+        return $controller('ModifyCategoryCtrl', {
+          $scope: $scope,
+          CategoryService: CategoryService
+        });
+      };
+    });
+  });
+
+  it('emit from parent controller', function() {
+    spyOn($scope, '$emit');
+    createController();
+    expect($scope.$emit).toHaveBeenCalledWith('parentManage');
+  });
+
+});
