21b883b1bb7cdf50ffdc4e2d800773be9713a12b
sorashiro <yufei930101@gmail.com>
2014-09-17 17:05:41 +0800

    add get correct number function test in index-spec.js

--- test/spec/controllers/index-spec.js
+++ test/spec/controllers/index-spec.js
@@ -1,30 +1,53 @@
 'use strict';
 
 describe('Controller: IndexCtrl', function () {
-  var ItemsService, $scope, createController;
+  var ItemsService, $scope, createController, $rootScope;
 
   beforeEach(function () {
     module('letusgoAngularJsApp');
 
     inject(function ($injector) {
 
-      $scope = $injector.get('$rootScope').$new();
+      $rootScope = $injector.get('$rootScope');
+      $scope = $rootScope.$new();
       ItemsService = $injector.get('ItemsService');
 
       var $controller = $injector.get('$controller');
 
       createController = function () {
-        return $controller('ItemsCtrl', {
+        return $controller('IndexCtrl', {
           $scope: $scope,
           itemService: ItemsService
         });
       };
     });
   });
+
   it('should load items', function () {
     spyOn(ItemsService, 'items');
     createController();
     ItemsService.items();
     expect(ItemsService.items).toHaveBeenCalled();
   });
+
+  it('emit from parent controller', function() {
+    spyOn($scope, '$emit');
+    createController();
+    expect($scope.$emit).toHaveBeenCalledWith('parentHome');
+    expect($scope.$emit).toHaveBeenCalledWith('parentCount');
+  });
+
+  it('should get correct number', function() {
+    createController();
+    $scope.$digest();
+    spyOn(ItemsService, 'get');
+
+    $rootScope.$broadcast('parentCount');
+    $scope.$digest();
+
+    var count = ItemsService.get() || 0;
+    expect($scope.count).toBe(count);
+  });
+
+
 });
