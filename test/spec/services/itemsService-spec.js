48200e3ab4461c3f30faa2f83d1594e1d41d601c
sorashiro <yufei930101@gmail.com>
2014-09-18 16:39:21 +0800

    add return itemsList test

---
+++ test/spec/services/itemsService-spec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('Controller: ItemsService', function () {
+  var $scope, localStorageService, ItemsService;
+
+  beforeEach(function () {
+    module('letusgoAngularJsApp');
+
+    inject(function ($injector) {
+
+      $scope = $injector.get('$rootScope').$new();
+      localStorageService = $injector.get('localStorageService');
+      ItemsService = $injector.get('ItemsService');
+    });
+
+    var item1 = {'category': 'fruit', 'name': 'apple', 'unit': '斤', 'price': '5.50'};
+    var item2 = {'category': 'fruit', 'name': 'leechee', 'unit': '斤', 'price': '15.00'};
+    var item3 = {'category': 'food', 'name': 'sprite', 'unit': '瓶', 'price': '3.00'};
+    var item4 = {'category': 'food', 'name': 'coca-cola', 'unit': '瓶', 'price': '3.00'};
+    var item5 = {'category': 'livingGoods', 'name': 'battery', 'unit': '个', 'price': '2.00'};
+    var items = [item1, item2, item3, item4, item5];
+  });
+
+  it('should return itemsList', function() {
+    spyOn(localStorageService, 'set');
+    var items = ItemsService.items();
+    expect(localStorageService.set).toHaveBeenCalled();
+    expect(items.length).toBe(5);
+    expect(items[0].name).toBe('apple');
+    expect(items[2].name).toBe('sprite');
+    expect(items[4].name).toBe('battery');
+  });
+  
+});
